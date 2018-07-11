fec7422812122278bb91214ee1a596fa03977741
Chad Lee <chad.lee@civicsource.com>
2018-07-11 11:46:47 -0500

    Print current version number to console
    Need to be able to debug which version of the tool is running from build server logs.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 
 import run from "./runner";
 import colors from "colors/safe";
+import { version } from "../package";
+
+console.log(`Workspace Versioner v${version}`);
 
 run().then(null, err => {
 	console.error(colors.red(err.message));
